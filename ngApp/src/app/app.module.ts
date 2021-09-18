ba0adf30b6923326090b360ca45287acdcd61e33
pocketknife <reynaldlawrence@outlook.ph>
2021-09-18 13:27:26 +0800

    frontend > components: profile, login, password backend > jwt-auth lib

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -9,9 +9,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, EmployeesComponent, EmployeeEditComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, EmployeesComponent, EmployeeEditComponent, LoginComponent, SignupComponent, ProfileComponent, RequestResetComponent, ResponseResetComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
