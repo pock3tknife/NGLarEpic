e7e10413e2ccd09ce84b6eed6a95948266196606
pocketknife <reynaldlawrence@outlook.ph>
2021-09-19 13:11:14 +0800

    backend > jwt config frontend > routes

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -4,9 +4,43 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { BeforeLoginService } from './services/before-login.service';
+import { AfterLoginService } from './services/after-login.service';
+
 const routes: Routes = [
   { path: 'employees', component: EmployeesComponent },
-  { path: 'edit/:id', component: EmployeeEditComponent },
+  { path: 'employeeedit/:id', component: EmployeeEditComponent },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [BeforeLoginService],
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    canActivate: [BeforeLoginService],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AfterLoginService],
+  },
+  {
+    path: 'request-password-reset',
+    component: RequestResetComponent,
+    canActivate: [BeforeLoginService],
+  },
+  {
+    path: 'response-password-reset',
+    component: ResponseResetComponent,
+    canActivate: [BeforeLoginService],
+  },
 ];
 
 @NgModule({
--- restful-api-laravel/routes/api.php
+++ restful-api-laravel/routes/api.php
@@ -34,4 +34,12 @@ Route::put('updateEmployee/{id}', 'EmployeeController@updateEmployee');
 // Delete Employee
 Route::delete('deleteEmployee/{id}', 'EmployeeController@deleteEmployee');
 
+Route::post('login', 'AuthController@login');
+Route::post('signup', 'AuthController@signup');
+Route::post('logout', 'AuthController@logout');
+Route::post('refresh', 'AuthController@refresh');
+Route::post('me', 'AuthController@me');
+Route::post('sendPasswordResetLink', 'ResetPasswordController@sendEmail');
+Route::post('resetPassword', 'ChangePasswordController@process');
+
 
